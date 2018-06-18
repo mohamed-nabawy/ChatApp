36bf275092a3acdc486c865ff0c4823149fd0733
mostafaelsayed <test@youremail.com>
2018-06-18 12:49:21 +0200

    responsive header

--- chat/frontend/areas/header.php
+++ chat/frontend/areas/header.php
@@ -1,18 +1,14 @@
-<link rel="icon" href="/ChatApp/chat/favicon.ico"> <!-- url relative to the main page not this template -->
+<nav class="navbar navbar-fixed-top navbar-inverse" style="position: fixed">
 
-<nav class="navbar navbar-fixed-top navbar-inverse" style="position: relative">
-	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-
+	<div class="container-fluid" id="myNavbar">
 
 		<ul class="nav navbar-nav navbar-right">
 
 			<li><a href="#">Home</a></li>
 
+			<li style="cursor: pointer;" ng-click="setAllNotificationsToRead($event)">
 
+				<a class="toggleChats">
 
 					<i class="fa fa-comments" ng-cloak>
 						<span ng-show="newLen > 0" ng-bind={{newLen}} class="badge1"></span>
@@ -20,24 +16,33 @@
 
 				</a>
 
+				<div style="width: 160px;position: absolute;margin-top: -5px;height: 300px;overflow-y: scroll;overflow-x: hidden;" ng-cloak ng-show="lastMessagesClicked == 1" class="panel wrapword" scroll-to-down>
 
+					<!-- the minus sign is for descending order -->
 					<div ng-repeat="m in messages | orderBy: '-messageId'" class="mes" ng-click="addChatWindow(m)">
 						<div ng-if="m.sentFrom != currentUser.id && m.new == 1" style="background-color: grey">
+							<div class="panel-heading" ng-bind="m.firstName" style="text-align: center;margin-left: 20px;"></div>
 
+			        		<div class="panel-body" ng-bind="m.content" style="text-align: center;margin-left: 20px;font-size: 8px;"></div>
 
 			        	</div>
 
 			        	<div ng-if="(m.sentFrom != currentUser.id && m.new == 0) || m.sentFrom == currentUser.id">
+							<div class="panel-heading" ng-bind="m.firstName" style="text-align: center;margin-left: 20px;"></div>
 
+			        		<div class="panel-body" ng-bind="m.content" style="text-align: center;margin-left: 20px;font-size: 8px;"></div>
 
 			        	</div>
 
+		        		<hr class="horiz" />
 		        		
 		        	</div>
 		        	
+		        </div>
 				
 			</li>
 
+			<li style="cursor: pointer;">
 
 				<a><i class="fa fa-bell"></i></a>
 
