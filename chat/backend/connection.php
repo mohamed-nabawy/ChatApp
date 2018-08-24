df4a3753670653fd2966ec55753123e2f359ba01
mostafaelsayed <mostafaelsayed9419@gmail.com>
2018-08-25 01:24:07 +0200

    update connection file

--- chat/backend/connection.php
+++ chat/backend/connection.php
@@ -1,15 +1,28 @@
 <?php
 	// make the parameters const so none can play with the connection
-	define("DB_SERVER", "localhost"); 
-	define("DB_USER", "root");
-	define("DB_PASS", "");
-	define("DB_NAME", "chat");
+	
+	if (strpos($_SERVER['REMOTE_ADDR'], '127.0.0') !== false) {
+		define("DB_SERVER", "localhost"); 
+		define("DB_USER", "root");
+		define("DB_PASS", "");
+		define("DB_NAME", "chat");
 
-    $conn = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME); // open connection
+	    $conn = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME); // open connection
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    
-    mysqli_set_charset($conn, 'utf8');
+	    if ($conn->connect_error) {
+	        die("Connection failed: " . $conn->connect_error);
+	    }
+	    
+	    mysqli_set_charset($conn, 'utf8');
+	}
+	else {
+		$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+		$server = $url["host"];
+		$username = $url["user"];
+		$password = $url["pass"];
+		$db = substr($url["path"], 1);
+
+		$conn = new mysqli($server, $username, $password, $db);
+	}
 ?>
